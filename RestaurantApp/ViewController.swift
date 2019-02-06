61f6e96d500039d01abbe4c4190530a3e0e90b9b
Wisse van Balveren <W.vanBalveren@student.han.nl>
2019-02-06 13:29:15 +0100

    Initial Commit

---
+++ RestaurantApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantApp
+//
+//  Created by Wisse van Balveren on 06/02/2019.
+//  Copyright © 2019 Wisse van Balveren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
